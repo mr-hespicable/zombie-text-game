f0ae8cd0b2f47ec1dd20765d898f7242edc66e5a
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2022-02-25 22:57:05 +0000

    Update main.py
    Making game a story-based game lmao turn based is way to hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-import sys
+import time, random, sys
 
 x = 1
 
@@ -42,6 +41,21 @@ while x == 1:
     cont = input('Continue? Y/N')
   if cont == 'Y':
     x = (x+1)
-    print("Program will be here")
+    print('A zombie apocalypse has broken out in England. You must reach the signal tower emitting the dangerous spores that are infecting the public. Unfortunately, the signal tower is located in the Ikea warehouse full of English zombies yelling' "Bloody foreigners!" 'Sadly, you are not from England, so you are at great risk to these zombies. You will have to make different choices that affect the story, with many resulting in death.'
+)
+
   elif cont == 'N':
     again = input('OK. Would you like to CHOOSE AGAIN, or EXIT the game?')
+    if again == 'CHOOSE AGAIN':
+      time.sleep(1)
+      print(' ')
+      print('OK. Choosing again.')
+      print(' ')
+      time.sleep(1)
+    elif again == 'EXIT':
+      time.sleep(0.5)
+      print(' ')
+      print('OK. Exiting.')
+      print(' ')
+      time.sleep(0.5)
+      quit()
