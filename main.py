07e6e840be1f7eae81006ba32ebb7c967c423ea8
mr-hespicable <85052021+mr-hespicable@users.noreply.github.com>
2022-02-26 09:07:22 +0000

    Update main.py
    formatted the messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, random, sys
+import time, random, sys, os
 
 x = 1
 
@@ -17,32 +17,43 @@ while x == 1:
     print ("Strengths - Extreme Studying Sweat") # knows lots of things by memory
     print ("Weaknesses - Says MATH") # communication skills are limited
     print ("Special ability - Window Wiping Wave") # defense mechanism
-    cont = input('Continue? Y/N')
+    cont = input('Continue? Y/N \n')
   
   elif char == 2:
    print('Leon Stats:')
    print ("Strengths - Brain") # can take advantage of situation better
    print ("Weaknesses - Bounces") # less agility
    print ("Special ability - Pickpocketing") # able to steal things with less risk of getting caught
-   cont = input('Continue? Y/N')
+   cont = input('Continue? Y/N \n')
   
   elif  char == 3:
    print('Harris Stats:')
    print ("Strengths - Plays Two Instruments") #able to confuse zombies with instruments.
    print ("Weaknesses - Wears Tinted Janitor Glasses") # cannot see as well
    print ("Special ability - plays obscure third instrument") # 
-   cont = input('Continue? Y/N')
+   cont = input('Continue? Y/N \n')
   
   elif char == 4:
     print ("Kalen Stats:")
     print ("Strengths = Head Boy")
     print ("Weaknesses = No PS4")
     print ("Special ability - can take control of situation")
-    cont = input('Continue? Y/N')
+    cont = input('Continue? Y/N \n')
   if cont == 'Y':
     x = (x+1)
-    print('A zombie apocalypse has broken out in England. You must reach the signal tower emitting the dangerous spores that are infecting the public. Unfortunately, the signal tower is located in the Ikea warehouse full of English zombies yelling' "Bloody foreigners!" 'Sadly, you are not from England, so you are at great risk to these zombies. You will have to make different choices that affect the story, with many resulting in death.'
-)
+    message1 = 'DECEMBER 14 1969\n'
+    for char in message1: # typewriter effect, very cool. took a really long time to figure out, had to watch several youtube videos explaining how sys works. i still don't get it fully.
+      sys.stdout.write(char)
+      sys.stdout.flush()
+      time.sleep(0.08)
+    
+    time.sleep(2)
+
+    message2 = '\nA zombie apocalypse has broken out in England. You must reach the signal tower emitting the dangerous spores that are infecting the public.\nUnfortunately, the signal tower is located in the Ikea warehouse full of English zombies yelling ' '"Bloody foreigners!"' ' Sadly, you are not from England, so you are at great risk to these zombies. You will have to make different choices that affect the story, with many resulting in death.'
+    for char in message2: 
+      sys.stdout.write(char)
+      sys.stdout.flush()
+      time.sleep(0.05)
 
   elif cont == 'N':
     again = input('OK. Would you like to CHOOSE AGAIN, or EXIT the game?')
